b4239ae780dd1e2fbda12272e3a71a488872f01b
minna <32199029+mshroom@users.noreply.github.com>
2018-03-29 19:54:59 +0300

    added user registration and corrected some typos

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, TextField
+from wtforms import PasswordField, TextField, validators
+
 
 class LoginForm(FlaskForm):
 	username = TextField("Username")
 	password = PasswordField("Password")
+	
+	class Meta:
+		csrf = False
+
+class RegisterForm(FlaskForm):
+
+	name = TextField("Name", [validators.Length(min=2, max=50, message="Name must contain 2 - 50 characters")])
+	username = TextField("Username", [validators.Length(min=2, max=50, message="Username must contain 2 - 50 characters")])
+	password = PasswordField("Password", [validators.Length(min=4, max=50, message="Password must contain 4 - 50 characters")])
 	
 	class Meta:
 		csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -23,3 +23,23 @@ def auth_login():
 def auth_logout():
 	logout_user()
 	return redirect(url_for("index"))
+
+@app.route("/auth/register", methods = ["GET", "POST"])
+def auth_register():
+	if request.method == "GET":
+		return render_template("auth/registerform.html", form = RegisterForm())
+	
+	form = RegisterForm(request.form)
+	
+	if not form.validate():
+		return render_template("auth/registerform.html", form = form)
+
+	user = User.query.filter_by(username=form.username.data).first()
+	if user:
+		return render_template("auth/registerform.html", form = form, error = "Username is already in use")
+	
+	u = User(form.name.data, form.username.data, form.password.data)
+	db.session().add(u)
+	db.session().commit()
+	return redirect(url_for("auth_login"))
+
